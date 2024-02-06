f6a059d72fbc8a539927b7d792956b728ea50b43
YoonSung Kim <opqudk@gmail.com>
2024-02-06 10:50:36 +0900

    🚑 HOTFIX: modile (#10)
    remove resolution check
    
    resolving: #10

--- src/components/Custom/CustomCanvasLayerRenderer.ts
+++ src/components/Custom/CustomCanvasLayerRenderer.ts
@@ -49,33 +49,36 @@ export default class CustomCanvasLayerRenderer extends Observable {
   prepareFrame(frameState: FrameState) {
     const {
       canvases,
-      previousFrame: { canvasId, centerX, centerY, resolution },
+      // previousFrame: { canvasId, centerX, centerY, resolution },
+      previousFrame: { canvasId, centerX, centerY },
     } = this;
     const previousCanvas = canvases[canvasId];
-    const resized = resizeCanvasIfNeeded(frameState, previousCanvas);
+    // const resized = resizeCanvasIfNeeded(frameState, previousCanvas);
+    resizeCanvasIfNeeded(frameState, previousCanvas);
     const nextResolution = frameState.viewState.resolution;
     const [currentCenterX, currentCenterY] = frameState.viewState.center;
 
     let nextCanvas = previousCanvas;
     let nextCanvasId = canvasId;
 
-    if (!resized && resolution === nextResolution) {
-      tmpPreviousCenterPixel = [centerX, centerY];
-      apply(frameState.coordinateToPixelTransform, tmpPreviousCenterPixel);
+    // if (!resized && resolution === nextResolution) {
+    // if (resolution === nextResolution) {
+    tmpPreviousCenterPixel = [centerX, centerY];
+    apply(frameState.coordinateToPixelTransform, tmpPreviousCenterPixel);
 
-      const dx = tmpPreviousCenterPixel[0] - frameState.size[0] / 2;
-      const dy = tmpPreviousCenterPixel[1] - frameState.size[1] / 2;
+    const dx = tmpPreviousCenterPixel[0] - frameState.size[0] / 2;
+    const dy = tmpPreviousCenterPixel[1] - frameState.size[1] / 2;
 
-      if (dx !== 0 || dy !== 0) {
-        nextCanvasId = (nextCanvasId + 1) % 2;
-        nextCanvas = canvases[nextCanvasId];
-        resizeCanvasIfNeeded(frameState, nextCanvas);
+    if (dx !== 0 || dy !== 0) {
+      nextCanvasId = (nextCanvasId + 1) % 2;
+      nextCanvas = canvases[nextCanvasId];
+      resizeCanvasIfNeeded(frameState, nextCanvas);
 
-        const newContext = nextCanvas.getContext('2d');
-        newContext?.clearRect(0, 0, nextCanvas.width, nextCanvas.height);
-        newContext?.drawImage(previousCanvas, dx, dy);
-      }
+      const newContext = nextCanvas.getContext('2d');
+      newContext?.clearRect(0, 0, nextCanvas.width, nextCanvas.height);
+      newContext?.drawImage(previousCanvas, dx, dy);
     }
+    // }
 
     this.previousFrame = {
       canvasId: nextCanvasId,
